1d350742c408976997f37c315d350283e933a13d
Maicon Gavazzoni <maicon@datasystemweb.com.br>
2022-03-27 13:32:45 -0300

    teste

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { RFValue } from 'react-native-responsive-fontsize';
-import {ActivityIndicator, Alert, Platform} from 'react-native';
+import { ActivityIndicator, Alert, Platform } from 'react-native';
 
-import { useTheme} from 'styled-components';
+import { useTheme } from 'styled-components';
 
 import AppleSvg from '../../assets/apple.svg';
 import GoogleSvg from '../../assets/google.svg';
 import LogoSvg from '../../assets/logo.svg';
-import { SignInSocialButton} from '../../components/SignInSocialButton';
+import { SignInSocialButton } from '../../components/SignInSocialButton';
 
 import { useAuth } from '../../hooks/auth';
 
@@ -21,24 +21,24 @@ import {
   FooterWrapper
 } from './styles';
 
-export function SignIn(){
+export function SignIn() {
   const [isLoading, setIsLoading] = useState(false);
-  const {signInWithGoogle, signInWithApple} = useAuth();
+  const { signInWithGoogle, signInWithApple } = useAuth();
   const theme = useTheme();
-  
-  async function handleSignInWithGoogle(){
-   
+
+  async function handleSignInWithGoogle() {
+
     try {
       setIsLoading(true);
       return await signInWithGoogle();
     } catch (error) {
       Alert.alert('Não foi possível conectar a conta google');
       setIsLoading(false);
-    }           
+    }
   }
 
-  async function handleSignInWithApple(){
-   
+  async function handleSignInWithApple() {
+
     try {
       setIsLoading(true);
       return await signInWithApple();
@@ -48,50 +48,50 @@ export function SignIn(){
     }
   }
 
-  return(
+  return (
     <Container>
       <Header>
-      <TitleWrapper>
-      <LogoSvg 
-        width={RFValue(120)}
-        height={RFValue(68)}
-       />
-       <Title>
-        Controle suas {'\n'} 
-        finanças de forma {'\n'} 
-        muito simples
-       </Title>
-       </TitleWrapper>
-       <SignInTitle>
-        Faça sue login com {'\n'} 
-        uma das contras abaixo
-       </SignInTitle>
+        <TitleWrapper>
+          <LogoSvg
+            width={RFValue(120)}
+            height={RFValue(68)}
+          />
+          <Title>
+            Controle suas {'\n'}
+            finanças de forma {'\n'}
+            muito simples
+          </Title>
+        </TitleWrapper>
+        <SignInTitle>
+          Faça sue login com {'\n'}
+          uma das contras abaixo
+        </SignInTitle>
       </Header>
 
       <Footer>
         <FooterWrapper>
 
           <SignInSocialButton
-          title='Entrar com Google'
-          svg={GoogleSvg}
-          onPress={handleSignInWithGoogle}
+            title='Entrar com a conta Google'
+            svg={GoogleSvg}
+            onPress={handleSignInWithGoogle}
           />
 
-         { Platform.OS === 'ios' &&
-          <SignInSocialButton
-          title='Entrar com Apple'
-          svg={AppleSvg}
-          onPress={handleSignInWithApple}
-          />
+          {Platform.OS === 'ios' &&
+            <SignInSocialButton
+              title='Entrar com Apple'
+              svg={AppleSvg}
+              onPress={handleSignInWithApple}
+            />
           }
         </FooterWrapper>
-        { isLoading && <ActivityIndicator 
-        color={theme.colors.shape} 
-        style={{marginTop:18}}
-        
+        {isLoading && <ActivityIndicator
+          color={theme.colors.shape}
+          style={{ marginTop: 18 }}
+
         />}
       </Footer>
-  
+
     </Container>
   );
 }
